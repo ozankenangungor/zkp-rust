dd743252dc4041a0f0c788a0fdb11ce9d6be504b
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-09-27 11:25:35 +0300

    Adding unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -129,4 +129,40 @@ mod tests {
             "Verification should have failed with a fake secret."
         );
     }
+
+    #[test]
+    fn test_schnorr_with_1024_bit_constants() {
+        use hex;
+
+        let p_hex = "B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975FD1247761E5C5A458E8803F6D115886E2D3B9F46D9B975B1466E22BF494602C973E068B202B12A3D1B6D0B6E1F3ED40B2778E251D83B0645070968068F3F57726DD1CA2AD9433DE22D94203F3A07CC3";
+        let p = BigUint::from_bytes_be(&hex::decode(p_hex).unwrap());
+
+        let q_hex = "F518AA8781A8A6508C3099A156CB919584F81B83";
+        let q = BigUint::from_bytes_be(&hex::decode(q_hex).unwrap());
+
+        let alpha_hex = "A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266A047405B28952B21D627A693A6AE14F182B15C4828E679824B1417FD69C62157B5897E5425656DEC238EC8FEDFB6B84136437166145018BF1B6D9008AD4368D84A44664DE8E5912EDBE5ED449561E6F0BB805C4B594B8123B89AB6B01B054B";
+        let alpha = BigUint::from_bytes_be(&hex::decode(alpha_hex).unwrap());
+
+        let random_power = generate_random_below(&q);
+        let beta = alpha.modpow(&random_power, &p);
+
+        let protocol = SchnorrProtocol::new(p.clone(), q.clone(), alpha.clone(), beta.clone());
+
+        let x = generate_random_below(&q);
+        let k = generate_random_below(&q);
+        let c = generate_random_below(&q);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+        let s = protocol.solve(&k, &c, &x);
+
+        let result = protocol.verify(&r1, &r2, &y1, &y2, &c, &s);
+
+        assert!(
+            result,
+            "Doğrulama, 1024-bit'lik güvenli sabitlerle bile başarılı olmalıydı."
+        );
+    }
 }
