2939798bdf1459813294f3185ac3dc2ca70a48dd
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-09-27 06:38:37 +0300

    Correct verification logic and add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -41,3 +41,73 @@ pub fn verify(
 
     cond1 && cond2
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use num_bigint::BigUint;
+
+    #[test]
+    fn test_schnorr_protocol_happy_path() {
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32);
+        let k = BigUint::from(7u32);
+
+        let c = BigUint::from(4u32);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+
+        assert_eq!(r1, BigUint::from(8u32));
+        assert_eq!(r2, BigUint::from(4u32));
+
+        let s = solve(&k, &c, &x, &q);
+
+        assert_eq!(s, BigUint::from(5u32));
+
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+
+        assert!(
+            result,
+            "Validation should have been successful with correct inputs."
+        );
+    }
+
+    #[test]
+    fn test_schnorr_protocol_fake_secret_fails() {
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32);
+        let k = BigUint::from(7u32);
+        let y1 = exponentiate(&alpha, &x, &p); // y1 = 2
+        let y2 = exponentiate(&beta, &x, &p); // y2 = 3
+        let r1 = exponentiate(&alpha, &k, &p); // r1 = 8
+        let r2 = exponentiate(&beta, &k, &p); // r2 = 4
+
+        let c = BigUint::from(4u32);
+
+        let fake_secret = BigUint::from(7u32);
+
+        let fake_s = solve(&k, &c, &fake_secret, &q);
+
+        let fake_result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &fake_s, &p);
+
+        assert!(
+            !fake_result,
+            "The verification should have failed with a fake secret."
+        );
+    }
+}
