cc86fb0d755edaacc171eb9d500fff6ea60452d7
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-09-28 15:49:13 +0300

    Tonic Server

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,53 @@
-use tonic::{Code, Request, Response, Status, transport::Server};
+use tonic::{Request, Response, Status, transport::Server};
 
 pub mod zkp_auth {
     include!("./zkp_auth.rs");
 }
 
-use zkp_auth::auth_server;
+use zkp_auth::{
+    AuthenticationAnswerRequest, AuthenticationAnswerResponse, AuthenticationChallengeRequest,
+    AuthenticationChallengeResponse, RegisterRequest, RegisterResponse,
+    auth_server::{Auth, AuthServer},
+};
 
-fn main() {
-    println!("Hi, I am the server");
+#[derive(Debug, Default)]
+struct AuthImpl {}
+
+#[tonic::async_trait]
+impl Auth for AuthImpl {
+    async fn register(
+        &self,
+        request: Request<RegisterRequest>,
+    ) -> Result<Response<RegisterResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_authentication_challenge(
+        &self,
+        request: Request<AuthenticationChallengeRequest>,
+    ) -> Result<Response<AuthenticationChallengeResponse>, Status> {
+        todo!()
+    }
+
+    async fn verify_authentication(
+        &self,
+        request: Request<AuthenticationAnswerRequest>, // Corrected
+    ) -> Result<Response<AuthenticationAnswerResponse>, Status> {
+        todo!()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:50051".to_string();
+
+    println!("✅ Running the server in {}", addr);
+
+    let auth_impl = AuthImpl::default();
+
+    Server::builder()
+        .add_service(AuthServer::new(auth_impl))
+        .serve(addr.parse().expect("could not convert address"))
+        .await
+        .unwrap();
 }
